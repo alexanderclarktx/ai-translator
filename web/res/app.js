6882cd336deed52d51ce48bc10da8582b0acdbae
Alex <petrovich.alex96@gmail.com>
2026-02-25 13:47:43 -0600

    wip

--- web/res/app.js
+++
@@ -1,7 +0,0 @@
-// ../core/util/AppUtil.ts
-var formatUptime = (seconds) => {
-  const rounded = Math.floor(seconds);
-  return `${rounded}s`;
-};
-// app.ts
-console.log("hello world", formatUptime(10));
